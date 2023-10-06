93aeb18044953e310ba838a1ad5afb68cf896cb9
Rupendra <rupavbspu@gmail.com>
2023-10-06 10:57:02 +0530

    Add project files.

---
+++ profilio/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using profilio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace profilio.Controllers
+{
+    public class HomeController : Controller
+    {
+        PortfolioEntities db = new PortfolioEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Profilio()
+        {
+            return View();
+        } 
+        public ActionResult ProfilioFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ProfilioFile(datum dt)
+        {
+            dt.Crd = DateTime.Now;
+            db.data.Add(dt);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ profilio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="profilio.MvcApplication" Language="C#" %>
